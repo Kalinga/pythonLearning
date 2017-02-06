7d16230f329ecfb43e8e57222d5f5175c3b845b0
Kalinga <mail.kalinga@gmail.com>
2017-02-06 16:00:08 +0100

    assignment7 WIP

--- assignments/assignment7.py
+++ assignments/assignment7.py
@@ -2,6 +2,8 @@
 # coding=UTF-8
 import pandas as pd
 
+from datetime import datetime as dt
+
 import matplotlib.pyplot as plt
 
 from  testmodule.styleformat import heading
@@ -18,29 +20,34 @@ print df.Change.head()
 heading("Module 7 Assignments: 2 Rolling Mean of Adj.Close")
 print df["Adj Close"].mean()
 df['rolling_avg'] = pd.rolling_mean(df["Adj Close"], 100)
+
 #print df.loc[df.index, ["Adj Close", "rolling_avg"]]
 #print df[["Adj Close", "rolling_avg"]] Other Syntax
+
 print df.tail(20)
 print df['Adj Close'].max()
 print df[df["Adj Close"] == 245.4]
+
 #df['rolling_avg'].plot()
 #df['Adj Close'].plot()
 
-'''f, (ax, ay) = plt.subplots(1, 2, sharey=True)
-ax.plot(df['rolling_avg'])
+for i in df.index:
+    df['Date'][i] = dt.strptime( df['Date'][i], '%m/%d/%Y')
+
+f, (ax, ay) = plt.subplots(1, 2, sharey=True)
+ax.plot(df['Date'].values, df['rolling_avg'], 'y.')
 ax.set_title('rolling_avg')
-ay.plot(df['Adj Close'])
+ay.plot(df['Date'].values, df['Adj Close'])
 ay.set_title('Adj Close')
-plt.show()'''
+plt.show()
 
 print df['Date'].values
 print type(df['Date'][0])
 
+plt.plot(df['Date'].values, df['Adj Close'].values, 'b-', label='Adj Close')
+plt.plot(df['Date'].values, df['rolling_avg'].values, 'g-', label='rolling_avg')
 
+plt.legend(loc='upper right')
+plt.show()
 
-#plt.plot(df['Date'].values, df['rolling_avg'].values, 'y-', label='rolling_avg')
-#plt.plot(df['Adj Close'], 'r-', label='Adj Close')
-#plt.legend(loc='upper right')
-#plt.show()
-
-heading("Module 7 Assignments: 3 matrix using scipy")
+heading("Module 7 Assignments: 3 ")
