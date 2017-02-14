20f93aa9ebf2887ebeced4d8f91e5890fa846d7d
Kalinga <mail.kalinga@gmail.com>
2017-02-14 06:59:09 +0100

    assignment 10 imdb scraping for top 250 movies

--- assignments/assignment10_imdb.py
+++ assignments/assignment10_imdb.py
@@ -5,6 +5,8 @@ import urllib2 as url2
 
 from testmodule.styleformat import heading
 
+#Tag, NavigableString, BeautifulSoup, and Comment. These are the four types
+#of Object that we usually deal with while parsing using beautifulsoup
 heading("Module 10 Assignments: 2web scraping program to display \
          top 250 movies rated in IMDB.com. Output should be in the below format:")
 
@@ -20,7 +22,7 @@ def parse_soup(soup_contents):
 
     tbody= soup_contents.find('tbody')
     if has_class(tbody):
-        print "class value: ",
+        print "class =  ",
         print tbody['class']
     #print tbody
 
@@ -31,22 +33,43 @@ def parse_soup(soup_contents):
 
         tbody = soup_contents.find_next('tbody')
 
-    return parse_table_data(tbody)
+    return parse_table_data(lister_list_tbody)
 
-def parse_table_data(tbody):
-    pass
+def parse_table_data(table):
+    movies = list()
+    for row in table.find_all('tr'):
+        movie = list()
+        #print "-" * 30
+        for col in row.find_all('td'):
+            #print "*"* 30
+            #print type(col)
+            #print col
+            #print col['class']
+            if 'titleColumn' in col['class']:
+                contents = col.contents
+                movie.append((contents[0]).strip())
+                movie.append(contents[1].string)
+                movie.append(contents[3].string)
+        movies.append(movie)
+    return movies
 
 def getWebsite(argument):
     print "Fetching.. "+ (argument)
     html = url2.urlopen(argument)
+
     s = soup(html)
     #print s
-    links = parse_soup(s)
-    for index, value in enumerate(links):
-        print "Link " + str(index + 1) + ": " + value
+    movies_list = parse_soup(s)
+
+    print "Sl. Name. Year"
+    for index, value in enumerate(movies_list):
+        len(value)
+        print  value[0],
+        print  value[1],
+        print  value[2]
 
 if __name__ == "__main__":
-    print "http_proxy and https_proxy env variables are properly set and reflected in pycharm"
+    print "Attention: http_proxy and https_proxy env variables are properly set and reflected in pycharm"
     url_from_browser = "http://www.imdb.com/chart/top?ref_=nv_mv_250_6"
     getWebsite(url_from_browser)
 
