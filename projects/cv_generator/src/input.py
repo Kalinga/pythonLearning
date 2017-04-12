991e5638b986061b30c92564d92f575773e2c0a0
Kalinga <kalingabhusan.ray@in.bosch.com>
2017-04-12 07:37:23 +0200

    Refine Cover

---
+++ projects/cv_generator/src/input.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+cv_name = 'KALINGA_RAY_2017'
+contact_email = "mailto:mail.kalinga@gmail.com"
+contact_name = "Kalinga Bhusan Ray"
+
+html_file = "../gen/" + cv_name + '.html'
+pdf_file = "../gen/" + cv_name + '.pdf'
+cover = "../gen/" + 'cover.html'
+
+manager_name = ''
+company_name = 'Valeo'
+
+#Complete Address
+company_long_name = 'Valeo Schalter und Sensoren GmbH'
+company_address = 'Laiernstrasse 12'
+company_zip_town = '74321 Bietigheim-Bissingen'
+company_state_country = 'Baden-Württemberg, Germany'
