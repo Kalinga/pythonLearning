a339d8f86bc2e08c1b35c61dedb332593c884575
Kalinga BHusan Ray <mail.kalinga@gmail.com>
2019-04-26 20:49:38 +0200

    Update for 2019

--- projects/cv_generator/src/input.py
+++ projects/cv_generator/src/input.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-cv_name = 'KALINGA_RAY_2017'
+cv_name = 'KALINGA_RAY_2019'
 contact_email = "mailto:mail.kalinga@gmail.com"
 contact_name = "Kalinga Bhusan Ray"
 
@@ -12,10 +12,11 @@ company_name = ""
 
 #Complete Address
 
+
 company_long_name = ''
 company_address = ''
 company_zip_town = ''
-company_state_country = 'Deutschland'
+company_state_country = ''
 
 DE=False
 
@@ -25,7 +26,7 @@ def mailBody():
 
     Please find the attached Profile matching the advertised job along with the cover letter.
 
-    The most recent profile can be found at: https://github.com/Kalinga/pythonLearning/blob/master/projects/cv_generator/cv/KALINGA_RAY_2017.pdf
+    The most recent profile can be found at: https://github.com/Kalinga/pythonLearning/blob/master/projects/cv_generator/cv/KALINGA_RAY_2019.pdf
 
     Best Regards,
     Kalinga
